8a3f68d17662264d5ba3581037fe0fe8e813c638
meetsiraja <meetsiraja@gmail.com>
2022-12-28 12:46:47 +0530

    ft: liquidation bot v2

---
+++ v2/index.js
@@ -0,0 +1,67 @@
+const Web3 = require("web3");
+const Provider = require("@truffle/hdwallet-provider");
+const ABI = require("./config/abi.json");
+require('dotenv').config()
+
+const WEB3_HTTP_NODE_URL = process.env.nodeHttpUrl;
+const WEB3_WSS_NODE_URL = process.env.nodeWssUrl;
+
+const ACCOUNT_MANAGER_ADDRESS = "0x24aa4914f476526aF9Eac4B9c5B9C090FCEfe5AB";
+
+//Private Key and address for admin wallet which holds the UND balance
+const adminAddress = "0x07551C8c7b83f8Ea04c01cdb55686e59d5A24a1D";
+const privateKey = process.env.privateKey; // without 0x
+
+const web3Http = new Web3(new Provider(privateKey, WEB3_HTTP_NODE_URL));
+const web3Wss = new Web3(new Web3.providers.WebsocketProvider(WEB3_WSS_NODE_URL));
+
+const accountManager = new web3Http.eth.Contract(ABI, ACCOUNT_MANAGER_ADDRESS)
+
+async function main() {
+
+    let lock = false
+
+    web3Wss.eth.subscribe('newBlockHeaders', (error, block) => {
+
+        if (!error) {
+            console.log(`New Block Mined: ${block.number}`)
+
+            if (lock == false) {
+                lock = true
+
+                try {
+
+                    accountManager.methods.liquidateAccounts(1).estimateGas({ from: adminAddress }, function (etimateErr, gasAmount) {
+                        if (etimateErr) {
+                            if (!etimateErr.toString().includes("AccountManager: nothing to liquidate")) {
+                                console.log({ etimateErr })
+                            }
+                        } else {
+                            console.log("Found liquitable account....")
+                            accountManager.methods.liquidateAccounts(1)
+                                .send({ from: adminAddress, gas: gasAmount }, function (txErr, transactionHash) {
+                                    if (txErr) {
+                                        console.log("Error while executing transaction: ")
+                                        console.log({ txErr })
+                                    } else {
+                                        console.log("Liquidation Transaction sent: ", transactionHash)
+                                    }
+                                })
+                        }
+                    })
+
+                } catch (catchErr) {
+                    console.log({ catchErr })
+                }
+
+                lock = false
+            }
+
+        } else {
+            console.log({ error })
+        }
+
+    })
+
+}
+main()
